ada3c113909fb21d9aea38fa22b6f3f77114b8c4
yaseen499 <yaseenvlkr@gmail.com>
2025-06-21 11:04:21 +0530

    Add files via upload

---
+++ array folder/reverse-array/main.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+printf("reveresed array:");
+  for(int j=n-1;j>=0 ;j--){
+      printf("%d ",arr[j]);
+  }
+
+  
+
+    return 0;
+}
